a14e707f6ac55766c232b833975a1dfb3c6437af
kuriousdesign <gardner.761@gmail.com>
2024-03-19 14:37:40 -0600

    more changes

--- pages/jakes-house/HousePhotoSlide.js
+++ pages/jakes-house/HousePhotoSlide.js
@@ -8,8 +8,7 @@ const HousePhotoSlide = ({ url }) => {
     <div className="w-screen h-screen flex items-center justify-center">
       <div className="relative w-full h-full">
         <div className="absolute top-0 left-0 w-full h-full flex text-center justify-center items-center">LOADING...</div>
-        <Image src={url} fill="true" alt="" className="" />
-        
+        <Image src={url} alt="" layout="fill" objectFit="contain" className="w-full h-full max-w-full max-h-full" />
       </div>
     </div>
   );
