2a0a0dcb9d13b55406765c494428b94a2b1be995
kuriousdesign <gardner.761@gmail.com>
2025-05-14 03:36:33 -0700

    go eat some food ya bastard

--- pages/home/slides.js
+++ pages/home/slides.js
@@ -36,9 +36,9 @@ const slideFirst = (
 
 const slideArtMeetsRobotics = (
   <LayoutSlide className="bg-indigo-950">
-  <div className='w-3/4 max-w-[800px] flex flex-col justify-start items-center h-[70vh] mt-[15vh] mb-auto mx-[12vw] text-left'>
+  <div className='w-7/8 max-w-[900px] flex flex-col justify-start items-center h-[70vh] mt-[15vh] mb-auto mx-[12vw] text-left'>
     <SlideHeading text="Art meets Robotics" />
-      <p className='text-base md:text-lg'>
+      <p className='text-lg md:text-lg'>
         Let me help design your next interactive creation and inject some robotic horsepower.
         Who said engineers can&apos;t be artists?
       </p>
@@ -48,8 +48,8 @@ const slideArtMeetsRobotics = (
 
 const slideItsAlive = (
   <LayoutSlide className="bg-purple-900">
-  <div className='w-3/4 max-w-[800px] flex flex-col justify-start items-center h-[70vh] mt-[15vh] mb-auto mx-[12vw] text-left'>
-    <SlideHeading text={"It's Alive"} punctuation="!" />
+  <div className='w-7/8 max-w-[900px] flex flex-col justify-start items-center h-[70vh] mt-[15vh] mb-auto mx-[12vw] text-left'>
+  <SlideHeading text={"It's Alive"} punctuation="!" />
       <p className='text-base md:text-lg'>
         Bring some life to your bar, restaurant, venue, hotel or home.
         I can make your existing space more interactive and fun, even using your existing devices.
@@ -61,9 +61,9 @@ const slideItsAlive = (
 
 const slideGamify = (
   <LayoutSlide className="bg-purple-800">
-  <div className='w-3/4 max-w-[800px] flex flex-col justify-left items-left h-[70vh] mt-[15vh] mb-auto mx-[12vw] text-left'>
-    <SlideHeading text={"Gamify your Space"} punctuation="!" />
-      <p className='text-base md:text-lg'>
+  <div className='w-7/8 max-w-[900px] flex flex-col justify-start items-center h-[70vh] mt-[15vh] mb-auto mx-[12vw] text-left'>
+  <SlideHeading text={"Gamify your Space"} punctuation="!" />
+      <p className='text-lg md:text-lg'>
         With a little bit of code and some hardware, I can turn your space into a game.
         The budget is friendlier than you think.
       </p>
@@ -73,15 +73,15 @@ const slideGamify = (
 
 const slideServices = (
   <LayoutSlide className="bg-gray-800">
-    <div className='max-w-[800px] flex flex-col justify-start items-start h-[70vh] mt-[15vh] mb-auto mx-[20vw] text-left'>
-      <SlideHeading text="Jake-of-all-trades" />
-      <p className='text-base md:text-lg'>
+  <div className='w-7/8 max-w-[900px] flex flex-col justify-start items-center h-[70vh] mt-[15vh] mb-auto mx-[12vw] text-left'>
+  <SlideHeading text="Jake-of-all-trades" />
+      <p className='text-lg md:text-lg'>
         I&apos;m a creative engineer, roboticist, full-stack developer, and a Jake. Here is a list of the services I offer.
       </p>
  
-      <div className='mt-10 xl:mt-10 flex justify-center z-50'>
+      <div className='mt-10 xl:mt-10 flex justify-start z-50'>
         <Link href={'/services'}>
-          <button className="h-[37px] w-[100px] xl:h-[45px] xl:w-[130px] rounded-full ring-2 ring-white bg-white text-green-800 text-[10px] xl:text-xs font-bold font-permanentMarker hover:ring-redpinkAccent hover:text-redpinkAccent hover:bg-transparent hover:scale-105 ]">
+          <button className="h-[37px] w-[100px] xl:h-[45px] xl:w-[130px] rounded-full ring-2 ring-white bg-white text-gray-800 text-[10px] xl:text-xs font-bold font-permanentMarker hover:ring-redpinkAccent hover:text-redpinkAccent hover:bg-transparent hover:scale-105 ]">
             SERVICES
           </button>
         </Link>
@@ -92,9 +92,9 @@ const slideServices = (
 
 const slideActiveProjects = (
   <LayoutSlide className="bg-gray-800">
-  <div className='w-3/4 max-w-[800px] flex flex-col justify-start items-start h-[70vh] mt-[15vh] mb-auto mx-[12vw] text-left'>
-    <SlideHeading text="Recent Projects" />
-      <p className='text-base md:text-lg mb-10'>
+  <div className='w-7/8 max-w-[900px] flex flex-col justify-start items-center h-[70vh] mt-[15vh] mb-auto mx-[12vw] text-left'>
+  <SlideHeading text="Recent Projects" />
+      <p className='text-lg md:text-lg mb-10'>
         Kurious what I&apos;ve been up to... Here is what I&apos;ve been tinkering on.
       </p>
       <ActiveProjectsList />
@@ -106,9 +106,9 @@ const slideActiveProjects = (
 
 const slideReactiveLighting = (
   <LayoutSlide className=" bg-black">
-  <div className='w-3/4 max-w-[800px] flex flex-col justify-start items-center h-[70vh] mt-[15vh] mb-auto mx-[12vw] text-left'>
-    <SlideHeading text="Reactive Lighting" />
-    <p className='text-base md:text-lg'>
+  <div className='w-7/8 max-w-[900px] flex flex-col justify-start items-center h-[70vh] mt-[15vh] mb-auto mx-[12vw] text-left'>
+  <SlideHeading text="Reactive Lighting" />
+    <p className='text-lg md:text-lg'>
       Can&apos;t afford a lighting tech at your venue, but want an interesting light show anyways? Try out my reactive lighting controls with multiple modes to suite your genre.
     </p>
     <SpotlightAnimation />
@@ -119,12 +119,12 @@ const slideReactiveLighting = (
 
 const slideResume = (
   <LayoutSlide className=" bg-black">
-  <div className='max-w-[800px] flex flex-col justify-start items-start h-[70vh] mt-[15vh] mb-auto mx-[12vw] text-left'>
-    <SlideHeading text="About my Experience" />
-    <p className='text-base md:text-lg'>
+  <div className='w-7/8 max-w-[900px] flex flex-col justify-start items-center h-[70vh] mt-[15vh] mb-auto mx-[12vw] text-left'>
+  <SlideHeading text="About my Experience" />
+    <p className='text-lg md:text-lg'>
       Check out my work and academic background using my interactive resume.
     </p>
-    <div className='mt-10 flex justify-center z-50'>
+    <div className='mt-10 flex justify-left z-50'>
         <Link href={'/about/resume'}>
           <button className="h-[37px] w-[100px] xl:h-[45px] xl:w-[130px] rounded-full ring-2 ring-white bg-white text-green-800 text-[10px] xl:text-xs font-bold font-permanentMarker hover:ring-black hover:text-black hover:bg-transparent hover:scale-105 ]">
             RESUME
